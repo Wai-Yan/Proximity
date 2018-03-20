d53fb0a39c6941b0b4d9d779f4dbffeb54b6fd2d
tak009 <snalut@gmail.com>
2018-03-20 16:07:51 -0400

    build recruiter login

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,3 @@
-//Get data from front end and connect to OKTA create users APIs in routes folder
 $(document).ready(function() {
   $(document.body).on("click", "#loginModal #oktaLogin", function() {
     console.log("I'm here login");
@@ -10,6 +9,24 @@ $(document).ready(function() {
       password: newPassword,
     };
 
+    userLogin(newLogin);
+  });
+
+  $(document.body).on("click", "#recruiterModal #recruiterSubmit", function() {
+    console.log("recruiter login");
+    var newEmail = $("#recruiterModal #loginEmail_r").val().trim();
+    var newPassword = $("#recruiterModal #loginPassword_r").val().trim();
+
+    var newLogin = {
+      email: newEmail,
+      password: newPassword,
+    };
+
+    userLogin(newLogin);
+  });
+
+
+  function userLogin(loginObj) {
     // Bootstrap the AuthJS Client
     var authClient = new OktaAuth({
       url: 'https://dev-975345.oktapreview.com',
@@ -18,14 +35,13 @@ $(document).ready(function() {
     });
     // Attempt to retrieve ID Token from Token Manager
     var idToken = authClient.tokenManager.get('idToken');
-    console.log("Line 22" + idToken);
+    console.log("idToken " + idToken);
     // If ID Token exists, return it in console.log
     if (idToken) {
       console.log(`hi ${idToken.claims.email}!`);
 
       // If ID Token isn't found, try to parse it from the current URL
     } else if (location.hash) {
-      console.log("Line 28");
       authClient.token.parseFromUrl()
         .then(idToken => {
           console.log(`hi ${idToken.claims.email}!`);
@@ -35,9 +51,8 @@ $(document).ready(function() {
         });
     } else {
       // You're not logged in, you need a sessionToken
-      var username = newEmail;
-      var password = newPassword;
-      console.log("Line 38");
+      var username = loginObj.email;
+      var password = loginObj.password;
 
       authClient.signIn({
           username,
@@ -48,10 +63,11 @@ $(document).ready(function() {
           if (res.status === 'SUCCESS') {
             authClient.token.getWithRedirect({
               sessionToken: res.sessionToken,
-              responseType: 'id_token',
+              responseType: 'id_token'
             });
           }
         });
     }
-  });
+  }
+
 });
