1233a743765a336cdafd990ea304004529e93a20
tak009 <snalut@gmail.com>
2018-03-22 14:04:01 -0400

    add recruiter route

--- public/js/registration.js
+++ public/js/registration.js
@@ -1,9 +1,19 @@
-//Get data from front end and connect to OKTA create users APIs in routes folder
 $(document).ready(function() {
+
+  $(document.body).on("keypress", "#email_r", function() {
+    console.log("Register Tab");
+    $("#recruiterSubmit").addClass("bind-register-btn");
+    $("#recruiterSubmit").removeClass("bind-login-btn");
+  });
+
+  $(document.body).on("keypress", "#loginEmail_r", function() {
+    console.log("Register Tab");
+    $("#recruiterSubmit").addClass("bind-login-btn");
+    $("#recruiterSubmit").removeClass("bind-register-btn");
+  });
+
   $(document.body).on("click", "#registerModal #oktaRegister", function() {
-    //event.preventDefault();
     console.log("I'm here");
-    //var registerModal = $("#registerform");
     var newFirstname = $("#registerModal #firstname").val().trim();
     var newLastname = $("#registerModal #lastname").val().trim();
     var newEmail = $("#registerModal #email").val().trim();
@@ -12,6 +22,7 @@ $(document).ready(function() {
     var newPreferredLoc = $("#registerModal #preferredloc").val().trim();
     var newRadius = $("#registerModal #radius").find(':selected').data('size');
     var newRemote = $("#registerModal #remote").is(":checked");
+    var newUserType = "jobsearcher";
     var newImage; // = $("#registerModal #userImageUploadFile");
 
     var newRegistration = {
@@ -23,6 +34,7 @@ $(document).ready(function() {
       ploc: newPreferredLoc,
       radius: newRadius,
       remote: newRemote,
+      userType: newUserType,
       image: newImage
     };
 
@@ -33,10 +45,11 @@ $(document).ready(function() {
     $.post("/api/register", newRegistration).done(function(data) {
 
       if (data) {
-        console.log(data);
-        userOktaId = data.id;
-
-        $("#successModal").modal("show");
+        if(data.id !== undefined || data.id !== null){
+          userOktaId = data.id;
+          userLogin(newRegistration);
+        }
+        //$("#successModal").modal("show");
 
       } else {
         alert("Oop! something went wrong. Please try again soon.");
@@ -46,15 +59,8 @@ $(document).ready(function() {
         console.log("reset form");
         $(this).find('form').trigger('reset');
       });
-
-      // $('#registerModal').is(':hidden', function() {
-      //   console.log("reset form");
-      //   $(this).find('form').trigger('reset');
-      // });
-
     }).
     then(function() {
-
       var newUser = {
         firstName: newFirstname,
         lastName: newLastname,
@@ -76,4 +82,109 @@ $(document).ready(function() {
         });
     });
   });
+
+  $(document.body).on("click", ".bind-register-btn", function() {
+    console.log("I'm here - Recruiter");
+    var newFirstname = $("#recruiterModal #firstname_r").val().trim();
+    var newLastname = $("#recruiterModal #lastname_r").val().trim();
+    var newEmail = $("#recruiterModal #email_r").val().trim();
+    var newPassword = $("#recruiterModal #password_r").val().trim();
+    var newMobile = $("#recruiterModal #mobilephone_r").val().trim();
+    var newCompanyName = $("#recruiterModal #companyname").val().trim();
+    var newCompanyWebsite = $("#recruiterModal #companywebsite").val().trim();
+    var newUserType = "recruiter";
+    var newImage; // = getUserImage();
+
+    var newRegistration = {
+      firstName: newFirstname,
+      lastName: newLastname,
+      email: newEmail,
+      password: newPassword,
+      mobilePhone: newMobile,
+      companyname: newCompanyName,
+      companywebsite: newCompanyWebsite,
+      userType: newUserType,
+      image: newImage
+    };
+
+    console.log(newRegistration);
+
+    $.post("/api/register", newRegistration).done(function(data) {
+      console.log(data);
+      if (data) {
+        console.log("Back from Register");
+        if(data.id !== undefined || data.id !== null){
+          userOktaId = data.id;
+          userLogin(newRegistration);
+        }
+
+      } else {
+        alert("Oop! something went wrong. Please try again soon.");
+      }
+
+      $('#recruiterModal').on('hidden.bs.modal', function() {
+        console.log("reset form");
+        $(this).find('form').trigger('reset');
+      });
+    }).
+    then(function() {
+      var newUser = {
+        firstName: newFirstname,
+        lastName: newLastname,
+        isRecruiter: true,
+        wantsRemote: newRemote,
+        preferredLocation: newPreferredLoc,
+        radius: newRadius,
+        associatedJobs: "[]",
+        phoneNo: newMobile,
+        email: newEmail,
+        oktaNo: userOktaId
+      };
+
+      // Send to DB
+      $.ajax({
+        method: "POST",
+        url: "/api/users/",
+        data: newUser
+        });
+    });
+  });
+
+  function userLogin(loginObj) {
+    var authClient = new OktaAuth({
+      url: 'https://dev-975345.oktapreview.com',
+      clientId: '0oaebzwcf9OKAHbHh0h7'
+    });
+
+    console.log(loginObj.email);
+    console.log(loginObj.password);
+
+    authClient.signIn({
+        username: loginObj.email,
+        password: loginObj.password
+      })
+      .then(function(transaction) {
+        if (transaction.status === 'SUCCESS') {
+          console.log(transaction.user.profile.firstName);
+          console.log("usertype", loginObj.userType);
+          console.log(loginObj.userType === "recruiter");
+          var redirectURL;
+          if(loginObj.userType === "recruiter"){
+            redirectURL = 'http://localhost:8080/recruiter?token='+transaction.sessionToken+"&userid="+transaction.user.id+"&email="+loginObj.email+"&type="+loginObj.userType;
+          } else {
+            redirectURL = 'http://localhost:8080/authorizeduser?token='+transaction.sessionToken+"&userid="+transaction.user.id+"&email="+loginObj.email+"&type="+loginObj.userType;
+          }
+          //addProfileName(transaction.user.profile.firstName);
+          //$( ".dropdown-content" ).find("p").text("HI").attr("value");
+          console.log("URL", redirectURL);
+          authClient.session.setCookieAndRedirect(transaction.sessionToken, redirectURL); // Sets a cookie on redirect
+        } else {
+          throw 'We cannot handle the ' + transaction.status + ' status';
+        }
+      })
+      .fail(function(err) {
+        console.error(err);
+      });
+  }
+
 });
