33d6fdf3262e324552a3e4c8c3e0c66b76aef662
Sean Andersen <smandersen13@gmail.com>
2018-03-23 20:53:57 -0400

    ReadMe updates

--- public/js/jobSearcher.js
+++ public/js/jobSearcher.js
@@ -102,7 +102,7 @@ $(document).ready(function() {
               console.log(finalSearchQuary)
                                                   }
                                                 }
-                                            }
+        }
       }
       addList()
     })
